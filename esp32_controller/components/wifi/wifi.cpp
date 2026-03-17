5c14432116c099e93095f8baf4a65d7b66d75891
ross <ross.herberigs@gmail.com>
2026-03-17 17:58:28 +0100

    implemented wifi for espnow

---
+++ esp32_controller/components/wifi/wifi.cpp
@@ -0,0 +1,21 @@
+#include "wifi.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "nvs_flash.h"
+#include "esp_err.h"
+
+esp_err_t wifi_init(void)
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    wifi_init_config_t config = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&config));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE));
+    ESP_ERROR_CHECK(esp_wifi_set_protocol(WIFI_IF_STA, WIFI_PROTOCOL_11B | WIFI_PROTOCOL_11G | WIFI_PROTOCOL_11N | WIFI_PROTOCOL_LR));
+    return ESP_OK;
+}
--- esp32_controller/main/main.cpp
+++ esp32_controller/main/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include "wifi.h"
 
 extern "C" void app_main(void)
 {
-
+    ESP_ERROR_CHECK(wifi_init());
 }
