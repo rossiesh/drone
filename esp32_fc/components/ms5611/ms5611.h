dcb8bf8f2dc65606535c75eb1247e7ad56a10ac7
Ross iesh Herberigs <ross.herberigs@gmail.com>
2026-05-03 22:52:36 +0200

    started with ms5611 sensor setup

---
+++ esp32_fc/components/ms5611/ms5611.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "esp_err.h"
+#include "cstdint"
+
+struct BarometerData{
+    int32_t pressure;
+    int32_t temp;
+};
+
+esp_err_t ms5611_init(void);
+esp_err_t ms5611_read_data(BarometerData &data);
